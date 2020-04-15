f05d114ff036b7a971a259afed78dae623270ff9
Steve Min <stevekmin@gmail.com>
2020-04-15 16:09:19 -0700

    save session

--- APM-Start/src/app/products/product-list.component.ts
+++ APM-Start/src/app/products/product-list.component.ts
@@ -1,11 +1,17 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { IProduct } from "./product";
 @Component({
   selector: "pm-products",
   templateUrl: "./product-list.component.html",
+  styleUrls: ["./product-list.component.css"],
 })
-export class ProductListComponent {
+export class ProductListComponent implements OnInit {
   pageTitle: string = "Product List";
-  products: any[] = [
+  imageWidth: number = 50;
+  imageMargin: number = 2;
+  showImage: boolean = false;
+  listFiler: string = "cart";
+  products: IProduct[] = [
     {
       productId: 1,
       productName: "Leaf Rake",
@@ -37,4 +43,11 @@ export class ProductListComponent {
       imageUrl: "assets/images/hammer.png",
     },
   ];
+  toggleImage(): void {
+    this.showImage = !this.showImage;
+  }
+
+  ngOnInit(): void {
+    console.log("In Onit");
+  }
 }
